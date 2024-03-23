84b621dd3d483188babd7f8e2b3f9427048542b9
JuShapiro <yuliashepel19@gmail.com>
2024-03-23 01:36:24 +0100

    already done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,30 @@
-console.log("Form");
+const form = document.querySelector(".feedback-form");
+const emailInput = form.elements.email;
+const textInput = form.elements.message;
+const storageKey = "feedback-form-state";
+
+form.addEventListener("input", event => {
+  const inputValues = {
+    email: emailInput.value.trim(),
+    message: textInput.value.trim(),
+  };
+  localStorage.setItem(storageKey, JSON.stringify(inputValues));
+});
+
+function loadPage() {
+  const savedValues = JSON.parse(localStorage.getItem(storageKey)) || {};
+  emailInput.value = savedValues.email || "";
+  textInput.value = savedValues.message || "";
+}; 
+
+loadPage();
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  const parsedValues = JSON.parse(localStorage.getItem(storageKey)) || {};
+  if(parsedValues.email && parsedValues.message){
+    console.log(parsedValues);
+  }
+  localStorage.removeItem(storageKey);
+  form.reset();
+});
